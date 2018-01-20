873bc5f49865e8f241aa737d71c39e8b1c996dbb
Tonmoy Iskander <tonmoyiskander@Tonmoys-MacBook-Pro.local>
2018-01-20 17:45:00 -0500

    dsasdwer

---
+++ Ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 #not a lie
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print " He's %d pounds heavy" % my_weight
+print "Actually thats not too heavy."
+print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee" % my_teeth
+
+#this one is trick, trying to get it exactly right
+print "If I add %d, %d, and %d I get %d" % (my_age, my_height, my_weight, my_age + my_height+my_weight)
+print "my weight is %d lbd which is %d kg" %(my_weight, my_weight * 0.45359237)
