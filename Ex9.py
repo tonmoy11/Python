c27740591514c4730f1e3aecbb341d1dbbcc2f83
Tonmoy Iskander <tonmoyiskander@Tonmoys-MacBook-Pro.local>
2018-01-20 19:16:22 -0500

    asdwer

---
+++ Ex9.py
@@ -0,0 +1,15 @@
+#Here;s Some new stranfe stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: " , days
+print "HEre are the months: ", months
+
+print """
+There's something going on here.
+With three double quotes.
+We'll be a ble to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+
+"""
