2540063b2daab0395f7628f329c4f5ad00ba76d2
Tonmoy Iskander <tonmoyiskander@Tonmoys-MacBook-Pro.local>
2018-01-25 16:09:46 -0500

    sde

--- Ex13.py
+++ Ex13.py
@@ -6,3 +6,6 @@ print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+
+#type this in CL
+#▶ python ex13.py first 2nd 3rd
---
+++ Ex14.py
@@ -0,0 +1,24 @@
+from sys import argv
+
+script, user_name = argv
+#prompt  = '>laksd'
+#or
+prompt  = '>'
+
+
+print "Hi %s, I'm the %s script." % (user_name, script)
+print "I'd like to ask you a few questions."
+print "Do you like me %s?" % (user_name)
+likes = raw_input(prompt)
+
+print "Where do you live %s?" % user_name
+lives = raw_input(prompt)
+
+print "What kind of computer do you have?"
+computer = raw_input(prompt)
+
+print """
+Alright so you saud %r about liking me.
+You live in %r. Note sure where that is.
+And you have %r computer. Nice.
+""" % (likes, lives, computer)
